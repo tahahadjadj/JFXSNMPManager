8bad271b4729ef13ab05d7c92b9353950cde0149
Taha Hadjadj <tahahadjadj@gmail.com>
2016-12-16 12:43:05 +0100

    get example

--- src/snmp/manager/SnmpGetExample.java
+++ src/snmp/manager/SnmpGetExample.java
@@ -23,80 +23,76 @@ import org.snmp4j.smi.UdpAddress;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
 
-public class SnmpGetExample
-{
-  private static String  ipAddress  = "127.0.0.1";
+public class SnmpGetExample{
+    private static String ipAddress = "127.0.0.1";
 
-  private static String  port    = "8001";
+    private static String port = "8001";
 
-  // OID of MIB RFC 1213; Scalar Object = .iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0
-  private static String  oidValue  = ".1.3.6.1.2.1.1.1.0";  // ends with 0 for scalar object
+// OID of MIB RFC 1213; Scalar Object = .iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0
+    private static String oidValue = ".1.3.6.1.2.1.1.1.0"; // ends with 0 for scalar object
 
-  private static int    snmpVersion  = SnmpConstants.version1;
+    private static int snmpVersion = SnmpConstants.version1;
 
-  private static String  community  = "public";
+    private static String community="public";
+    public static void main(String[] args) throws Exception{
+        System.out.println("SNMP GET Demo");
+        
+        // Create TransportMapping and Listen
+        TransportMapping transport = new DefaultUdpTransportMapping();
+        transport.listen();
 
-  public static void main(String[] args) throws Exception
-  {
-    System.out.println("SNMP GET Demo");
+        // Create Target Address object
+        CommunityTarget comtarget = new CommunityTarget();
+        comtarget.setCommunity(new OctetString(community));
+        comtarget.setVersion(snmpVersion);
+        comtarget.setAddress(new UdpAddress(ipAddress + "/" + port));
+        comtarget.setRetries(2);
+        comtarget.setTimeout(1000);
 
-    // Create TransportMapping and Listen
-    TransportMapping transport = new DefaultUdpTransportMapping();
-    transport.listen();
+        // Create the PDU object
+        PDU pdu = new PDU();
+        pdu.add(new VariableBinding(new OID(oidValue)));
+        pdu.setType(PDU.GET);
+        pdu.setRequestID(new Integer32(1));
 
-    // Create Target Address object
-    CommunityTarget comtarget = new CommunityTarget();
-    comtarget.setCommunity(new OctetString(community));
-    comtarget.setVersion(snmpVersion);
-    comtarget.setAddress(new UdpAddress(ipAddress + "/" + port));
-    comtarget.setRetries(2);
-    comtarget.setTimeout(1000);
+        // Create Snmp object for sending data to Agent
+        Snmp snmp = new Snmp(transport);
 
-    // Create the PDU object
-    PDU pdu = new PDU();
-    pdu.add(new VariableBinding(new OID(oidValue)));
-    pdu.setType(PDU.GET);
-    pdu.setRequestID(new Integer32(1));
+        System.out.println("Sending Request to Agent...");
+        ResponseEvent response = snmp.get(pdu, comtarget);
 
-    // Create Snmp object for sending data to Agent
-    Snmp snmp = new Snmp(transport);
+        // Process Agent Response
+        if (response != null){
 
-    System.out.println("Sending Request to Agent...");
-    ResponseEvent response = snmp.get(pdu, comtarget);
+            System.out.println("Got Response from Agent");
+            PDU responsePDU = response.getResponse();
+            if (responsePDU != null){
 
-    // Process Agent Response
-    if (response != null)
-    {
-      System.out.println("Got Response from Agent");
-      PDU responsePDU = response.getResponse();
+                int errorStatus = responsePDU.getErrorStatus();
+                int errorIndex = responsePDU.getErrorIndex();
+                String errorStatusText = responsePDU.getErrorStatusText();
 
-      if (responsePDU != null)
-      {
-        int errorStatus = responsePDU.getErrorStatus();
-        int errorIndex = responsePDU.getErrorIndex();
-        String errorStatusText = responsePDU.getErrorStatusText();
+                if (errorStatus == PDU.noError){
 
-        if (errorStatus == PDU.noError)
-        {
-          System.out.println("Snmp Get Response = " + responsePDU.getVariableBindings());
-        }
-        else
-        {
-          System.out.println("Error: Request Failed");
-          System.out.println("Error Status = " + errorStatus);
-          System.out.println("Error Index = " + errorIndex);
-          System.out.println("Error Status Text = " + errorStatusText);
-        }
-      }
-      else
-      {
-        System.out.println("Error: Response PDU is null");
-      }
-    }
-    else
-    {
-      System.out.println("Error: Agent Timeout... ");
-    }
-    snmp.close();
-  }
+                    System.out.println("Snmp Get Response = " + responsePDU.getVariableBindings());
+                }
+                else{
+
+                    System.out.println("Error: Request Failed");
+                    System.out.println("Error Status = " + errorStatus);
+                    System.out.println("Error Index = " + errorIndex);
+                    System.out.println("Error Status Text = " + errorStatusText);
+                }
+            }
+            else{
+
+                System.out.println("Error: Response PDU is null");
+            }
+        }
+        else{
+
+            System.out.println("Error: Agent Timeout... ");
+        }
+        snmp.close();
+    }
 }
